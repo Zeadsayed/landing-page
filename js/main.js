5b3fb81939b7d1233c70b50a1bfd30b82179c108
Zeadsayed <zeadsayed5@gmail.com>
2023-02-07 18:41:03 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let img = document.querySelector('.img');
+
+let main = document.querySelector('body');
+
+function phones(src) {
+    img.src= src;
+}
+
+function colors(color) { 
+    main.style.background = color;
+ }
